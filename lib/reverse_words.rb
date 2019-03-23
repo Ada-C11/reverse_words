3769097ef037b8c20da4f66d281aebce9e19e8c6
Carla Bosco <carlabosco@gmail.com>
2019-03-23 15:43:21 -0700

    Completed method with time and space complexities

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,6 +1,32 @@
 # A method to reverse each word in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: linear, as the words in the string are reversed in place
+# Space complexity: O(1), as the iterators are updated without further need for space
 def reverse_words(my_words)
-  raise NotImplementedError
+  word_start = 0
+  word_end = 0
+  i = 0
+  unless my_words == nil
+    while i < my_words.length                         # iterate until the end of the string
+      while i < my_words.length && my_words[i] == " " # ignore empty spaces
+        i += 1
+      end
+
+      word_start = i                                  # i is set to the index of first char found
+      
+      while i < my_words.length && my_words[i] != " " # iterate until the end of the word
+        i += 1
+      end
+
+      word_end = i - 1                                
+
+      while word_end > word_start                     # swap letters starting from the outside
+        temp = my_words[word_start]
+        my_words[word_start] = my_words[word_end]
+        my_words[word_end] = temp
+        word_start += 1
+        word_end -= 1
+      end
+    end 
+  end
+  return my_words
 end
