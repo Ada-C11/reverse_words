3b168a2fa3ea042e817fd1d0dc72a7253b59538d
Kelly Downes <kedownes@gmail.com>
2019-03-27 13:01:23 -0700

    Implement reverse_words method

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,6 +1,32 @@
 # A method to reverse each word in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n is the number of characters in the input string
+# Space complexity: O(1)
 def reverse_words(my_words)
-  raise NotImplementedError
+  return nil if my_words.nil?
+
+  start_index = 0
+  end_index = 0
+  my_words.length.times do |i|
+    end_index = i
+    if my_words[end_index] == ' '
+      word_reverse(start_index, end_index - 1, my_words)
+      start_index = end_index + 1
+    elsif end_index == my_words.length - 1
+      word_reverse(start_index, end_index, my_words)
+    end
+  end
+  return my_words
+end
+
+def word_reverse(start_index, end_index, words)
+  i = start_index
+  j = end_index
+  while i < j
+    temp = words[i]
+    words[i] = words[j]
+    words[j] = temp
+    i += 1
+    j -= 1
+  end
+  return words
 end
