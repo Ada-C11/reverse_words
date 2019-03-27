b4f804018944d4402c813f2588ed29914758d880
Pauline Sauget <paulinevsauget@gmail.com>
2019-03-27 16:55:14 -0700

    Wrote method to reverse words in a sentence

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -2,5 +2,27 @@
 # Time complexity: ?
 # Space complexity: ?
 def reverse_words(my_words)
-  raise NotImplementedError
+  return nil if !my_words
+  return my_words if my_words.length == 1
+  i, j = 0, 0 
+  while i < my_words.length
+    i += 1 while my_words[i] == " "
+    j = i
+    j += 1 while my_words[j] != " " && my_words[j] != nil
+    if j != i
+      reverse_string(my_words, i, (j - 1))
+    end
+    i = j + 1
+  end
+end
+
+def reverse_string(my_words, starting, ending)
+  temp = ""
+  while starting < ending
+    temp = my_words[starting]
+    my_words[starting] = my_words[ending]
+    my_words[ending] = temp
+    starting += 1
+    ending -= 1
+  end
 end
