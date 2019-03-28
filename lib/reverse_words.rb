351c59e5f9e522af3c94263bbe6ec5407fe360cc
Shubha Rajan <shubharajan94@gmail.com>
2019-03-27 22:35:52 -0700

    removed debug statements
    also updated time complexity explanation

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,7 +1,8 @@
 require "pry"
 # A method to reverse each word in a sentence, in place.
 # Time complexity: O(n) where n is the length of the string.
-# While there is one layer of loop nesting, both loops use the same iterator, index
+# While there is one layer of loop nesting, both loops use the same iterator, index. 
+# The calls to string_reverse add at most 1/2 n more iterations, so the time complexity still remains O(n)
 # Space complexity: O(1). The method uses constant time because it mutates the string in place.
 def reverse_words(my_words)
   # check that my_words is not nil
@@ -36,7 +37,6 @@ end
 
 # modified version of string_reverse solution from last week's hw
 def string_reverse(my_string, start_index, end_index)
-  # binding.pry
   (((end_index + 1) - start_index) / 2).times do |i|
     temp = my_string[start_index + i]
     my_string[start_index + i] = my_string[end_index - (i + 1)]
