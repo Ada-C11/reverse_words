b268a6aac29ea952304e7addb3a71622b05370d3
Ariana Bray <aribray@gmail.com>
2019-03-28 10:31:58 -0700

    finished reverse words

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -2,5 +2,33 @@
 # Time complexity: ?
 # Space complexity: ?
 def reverse_words(my_words)
-  raise NotImplementedError
+    if my_words.nil?
+      return nil
+    end
+    i = 0
+    j = 0
+    while j < my_words.length
+      if my_words[i] == ' '
+        i += 1
+        j = i
+        next
+      end
+  
+      while j < my_words.length && my_words[j] != ' '
+        j += 1
+      end
+  
+      reverse(my_words, i, j - 1)
+      i = j
+    end
 end
+  
+  def reverse(array, low, high)
+    while (low < high)
+      temp = array[low]
+      array[low] = array[high]
+      array[high] = temp
+      low += 1
+      high -= 1
+    end 
+  end
