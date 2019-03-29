6b618b53adfb272b769590b6e53007b0e4fbca04
Tatiana Quintana <tatsqui@gmail.com>
2019-03-28 17:39:46 -0700

    renaming variables

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -7,24 +7,24 @@ def reverse_words(my_words)
   
   i = 0
   l = my_words.length
-  start = 0
-  finish = 0
+  start_char = 0
+  finish_char = 0
   
   while i < l 
     while my_words[i] == " "
       i += 1
     end
-    start = i
+    start_char = i
     while my_words[i] != " " && i < l
       i += 1 
     end
-    finish = (i -1) 
-    while start < finish
+    finish_char = (i -1) 
+    while start_char < finish_char
         temp = my_words[start]
-        my_words[start] = my_words[finish]
+        my_words[start_char] = my_words[finish_char]
         my_words[finish] = temp
-        start += 1
-        finish -= 1
+        start_char += 1
+        finish_char -= 1
     end
   end
   return my_words
