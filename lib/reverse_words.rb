67480725e2908d0649d2b58cca589a75ca3a318b
Kate Sandler <kate@katekatekate.net>
2019-03-29 18:39:09 -0700

    establishes methods for word reversal

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,6 +1,23 @@
-# A method to reverse each word in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
 def reverse_words(my_words)
-  raise NotImplementedError
+  i = 0
+  j = 0
+  while !my_words.nil? && j <= my_words.length
+    if my_words[j] == ' ' || j == my_words.length
+      little_end = i
+      big_end = j - 1
+      reverse_substring(my_words, little_end, big_end)
+      i = j + 1
+    end
+    j += 1
+  end
+end
+
+def reverse_substring(my_words, little_end, big_end)
+  while little_end < big_end
+    pushpin = my_words[little_end]
+    my_words[little_end] = my_words[big_end]
+    my_words[big_end] = pushpin
+    little_end += 1
+    big_end -= 1
+  end
 end
