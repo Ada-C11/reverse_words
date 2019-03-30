8de3e2d09cddce60270b729501bb95657d82fae6
Grace Shea <grace.m.shea@gmail.com>
2019-03-29 18:51:26 -0700

    going back to previous version

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -6,49 +6,42 @@ require "pry"
 
 def reverse_words(my_words)
   # raise NotImplementedError
-  if my_words
-    i = 0
+  return nil if my_words == nil
+  i = 0
 
-    while i < my_words.length
-      # finding first character of a word
-      until i != " "
-        i += 1
-      end
-      word_start = i
-      # finding last character of a word or end of sentence
-      until i == " " || i == my_words.length
-        i += 1
-        # binding.pry
-      end
-      word_end = i - 1
-      word = my_words[word_start..word_end]
-      # binding.pry
-      string_reverse(word)
+  while i < my_words.length
+    until i != " "
+      i += 1
     end
+    word_start = i
+    until i == " " || i == my_words.length
       i += 1
       # binding.pry
     end
-    return my_words
-  else
-    return nil
+    word_end = i - 1
+
+    word = my_words[word_start..word_end]
+    # binding.pry
+    string_reverse(word)
+    i = word_end + 1
+    # binding.pry
   end
+  return my_words
 end
 
 def string_reverse(word)
-  if word
-    i = 0
-    j = word.length - 1
+  return word if word == nil
 
-    while i < j
-      temp = word[i]
-      word[i] = word[j]
-      word[j] = temp
-      # binding.pry
-      i += 1
-      j -= 1
-    end
-    return word
-  else
-    return nil
+  i = 0
+  j = word.length - 1
+
+  while i < j
+    temp = word[i]
+    word[i] = word[j]
+    word[j] = temp
+    # binding.pry
+    i += 1
+    j -= 1
   end
+  return word
 end
