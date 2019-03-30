52f237f2d338244adb3d34db0d2e4e9371eb694b
Grace Shea <grace.m.shea@gmail.com>
2019-03-29 18:45:24 -0700

    stopping point

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -6,42 +6,49 @@ require "pry"
 
 def reverse_words(my_words)
   # raise NotImplementedError
-  return nil if my_words == nil
+  if my_words
+    i = 0
 
-  i = 0
-
-  while i < my_words.length
-    until i != " "
-      i += 1
+    while i < my_words.length
+      # finding first character of a word
+      until i != " "
+        i += 1
+      end
+      word_start = i
+      # finding last character of a word or end of sentence
+      until i == " " || i == my_words.length
+        i += 1
+        # binding.pry
+      end
+      word_end = i - 1
+      word = my_words[word_start..word_end]
+      # binding.pry
+      string_reverse(word)
     end
-    word_start = i
-    until i == " " || i == my_words.length
       i += 1
       # binding.pry
     end
-    word_end = i - 1
-
-    word = my_words[word_start..word_end]
-    # binding.pry
-    string_reverse(word)
-    i = word_end + 1
-    # binding.pry
+    return my_words
+  else
+    return nil
   end
 end
 
 def string_reverse(word)
-  return word if word == nil
+  if word
+    i = 0
+    j = word.length - 1
 
-  i = 0
-  j = word.length - 1
-
-  while i < j
-    temp = word[i]
-    word[i] = word[j]
-    word[j] = temp
-
-    i += 1
-    j -= 1
+    while i < j
+      temp = word[i]
+      word[i] = word[j]
+      word[j] = temp
+      # binding.pry
+      i += 1
+      j -= 1
+    end
+    return word
+  else
+    return nil
   end
-  return word
 end
