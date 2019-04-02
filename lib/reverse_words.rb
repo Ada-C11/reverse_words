36f5e719e38fe5db5517441c8a02d077e0908821
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-04-02 14:07:43 -0700

    Added space/time complexity

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,12 +1,13 @@
 # A method to reverse each word in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), where n is the number of chanracters in the string
+# Space complexity: O(1)
 def swap(starting_index, ending_index, my_words)
   for j in (starting_index..(starting_index + (ending_index - starting_index - 1) / 2))
     temp = my_words[j]
     my_words[j] = my_words[starting_index + ending_index - j]
     my_words[starting_index + ending_index - j] = temp
   end
+  return my_words
 end
 
 def reverse_words(my_words)
@@ -29,4 +30,4 @@ def reverse_words(my_words)
   return my_words
 end
 
-puts reverse_words("the quick brown fox jumped over the lazy dog")
+puts reverse_words("the quick brown fox jumped over the fence")
