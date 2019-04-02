45bb9c64aeefb77b1e7b9b2986bda1e6afdb3868
amyesh <amyesh08@gmail.com>
2019-04-02 13:40:44 -0700

    complete rev words

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,42 +1,39 @@
 # A method to reverse each word in a sentence, in place.
-# Time complexity: O(w * l + n), where w represents the
-# number of words in a 'my_words,' the l represents the
-# number of letters in each word, and n represents the
-# length of 'my_words.'
-# Space complexity: O(1), because while i create a temporary array
-# to hold and reverse the words within the sentence, the output is
-# still the original array.
+# Time complexity: Not sure on this, but I believe O(n^2),
+# where n represents the length of my_words.
+# Space complexity: O(1), because the letters are reversed in place.
+
 def reverse_words(my_words)
   if my_words == nil
     return my_words
-  else
-    words = my_words.split
-    for word in words
-      len = word.length
-      i = 0
-      (len / 2).times do
-        low = word[0 + i]
-        high = word[len - 1 - i]
-        x = low
-        word[0 + i] = high
-        word[len - 1 - i] = x
-        i += 1
+  end
+  len = my_words.length
+  i = 0
+  low = nil
+  high = nil
+  len.times do
+    if my_words[i] != " "
+      if my_words[i - 1] == " " || i == 0
+        low = i
+      elsif my_words[i + 1] == " " || i + 1 == len
+        high = i
       end
-      if words.length > 1
-        reversed_words = words.join
-      else
-        reversed_words = words[0].to_s
-      end
-      a = 0
-      b = 0
-      my_words.length.times do
-        if my_words[0 + a] != " "
-          my_words[0 + a] = reversed_words[0 + b]
-          b += 1
+      if low != nil && high != nil
+        j = 0
+        len2 = high - low + 1
+        (len2 / 2).times do
+          l = my_words[low + j]
+          h = my_words[high - j]
+          x = l
+          my_words[low + j] = h
+          my_words[high - j] = x
+          j += 1
         end
-        a += 1
+        high = nil
+        low = nil
       end
     end
-    return my_words
+    i += 1
   end
+  return my_words
 end
