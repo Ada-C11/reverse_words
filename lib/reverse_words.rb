5d72ff866532feb7945379a3f2fae35b4e0ca46a
ranuseh <43229938+ranuseh@users.noreply.github.com>
2019-04-02 19:44:05 -0700

    Update reverse_words.rb

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,7 +1,3 @@
-# A method to reverse each word in a sentence, in place.
-# Time complexity: O(n) - Linear with n being the number of characters
-# Space complexity: O(1)
-
 def reverse(word)
   length = word.length
   (length / 2).round.times do |i|
@@ -9,41 +5,25 @@ def reverse(word)
     word[i] = word[length - i - 1]
     word[length - i - 1] = temp
   end
-
   return word
 end
 
-def replace(word, sentence, beginning)
-  word.length.times do |r_index|
-    sentence[beginning + r_index] = word[r_index]
-  end
-
-  return sentence
-end
-
-def reverse_slice(sentence, beginning_pos, end_pos)
-  word = sentence[beginning_pos...end_pos]
-  reversed_word = reverse(word)
-  replace(reversed_word, sentence, beginning_pos)
-  
-  return sentence
-end
-
 def reverse_words(sentence)
-  if sentence == nil
-    return nil
-  end
-  beginning = 0
-  sentence_length = sentence.length
+  i = 0
+  start = 0
+  ending = 0
 
-  sentence_length.times do |pos|
-    if sentence[pos] == ' '
-      reverse_slice(sentence, beginning, pos)
-      beginning = pos + 1
+  while i < sentence.length
+    if sentence[i] == ' ' 
+      ending = i - 1
+      sentence[start..ending] = reverse(sentence[start..ending])
+      start = i + 1
     end
+    i += 1
   end
-
-  return reverse_slice(sentence, beginning, sentence_length)
+  ending = i
+   sentence[start..ending] = reverse(sentence[start..ending])
+  return sentence
 end
 
-puts reverse_words('I strive         to engineer the most efficient solutions')
+puts reverse_words('I strive  to engineer the most efficient solutions')
