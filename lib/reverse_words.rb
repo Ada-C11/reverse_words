392a90f9b558845aafa803578407768ce03341a2
Mina Shin <minamishin@hotmail.com>
2019-04-03 23:08:39 -0700

    test passed

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,42 +1,34 @@
 # A method to reverse each word in a sentence, in place.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), where n is length of my_words
+# Space complexity: O(1)
 require "pry"
 
-def reverse_words(my_words)
-  # each letter needs to move opposite direction
-  if my_words != nil
-    all_words_split = my_words.split(" ")
-    all_words_split.each do |word|
-      i = 0
-      j = -1
-      while i < (word.length) / 2
-        last_letter = word[i]
-        word[i] = word[j]
-        word[j] = last_letter
-        i += 1
-        j -= 1
-      end
-    end
-    #   binding.pry
-    return my_words.replace(all_words_split.join(" "))
-  else
-    return nil
+def partial_reverse(my_words, start_of_word, end_of_word)
+  i = start_of_word
+  j = end_of_word
+  while i < j
+    my_words[i], my_words[j] = my_words[j], my_words[i]
+    i += 1
+    j -= 1
   end
-  # need to do this x-length of times? x = number of letters in the word/string
 end
 
-#["hello,", "world"]
-# word = reverse_words("hello, world")
-# puts "#{word}"
+def reverse_words(my_words)
+  return nil if my_words == nil
+  i = 0
+  while i < my_words.length
+    while my_words[i] == " " && i < my_words.length
+      i += 1
+    end
 
-# i = 0
-# j = -1
-# while i < (my_words.length) / 2
-#   last_letter = my_words[i]
-#   my_words[i] = my_words[j]
-#   my_words[j] = last_letter
-#   i += 1
-#   j -= 1
-# end
-# return my_words
+    start_of_word = i
+
+    while my_words[i] != " " && i < my_words.length
+      i += 1
+    end
+
+    end_of_word = i - 1
+
+    partial_reverse(my_words, start_of_word, end_of_word)
+  end
+end
