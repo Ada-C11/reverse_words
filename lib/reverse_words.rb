3db8a9c565e53153dcf7d484525fe721e029c1c2
Mina Shin <minamishin@hotmail.com>
2019-04-03 19:28:42 -0700

    nil test passed

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -5,21 +5,24 @@ require "pry"
 
 def reverse_words(my_words)
   # each letter needs to move opposite direction
-
-  all_words_split = my_words.split(" ")
-  all_words_split.each do |word|
-    i = 0
-    j = -1
-    while i < (word.length) / 2
-      last_letter = word[i]
-      word[i] = word[j]
-      word[j] = last_letter
-      i += 1
-      j -= 1
+  if my_words != nil
+    all_words_split = my_words.split(" ")
+    all_words_split.each do |word|
+      i = 0
+      j = -1
+      while i < (word.length) / 2
+        last_letter = word[i]
+        word[i] = word[j]
+        word[j] = last_letter
+        i += 1
+        j -= 1
+      end
     end
+    #   binding.pry
+    return my_words.replace(all_words_split.join(" "))
+  else
+    return nil
   end
-  #   binding.pry
-  return my_words.replace(all_words_split.join(" "))
   # need to do this x-length of times? x = number of letters in the word/string
 end
 
