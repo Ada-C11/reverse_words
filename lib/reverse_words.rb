6319b580cbe7827664815b450638d4f763bbcb4c
Mina Shin <minamishin@hotmail.com>
2019-04-03 18:54:02 -0700

    moved letters in opposite direction

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,6 +1,21 @@
 # A method to reverse each word in a sentence, in place.
 # Time complexity: ?
 # Space complexity: ?
+
 def reverse_words(my_words)
-  raise NotImplementedError
+  # each letter needs to move opposite direction
+  i = 0
+  j = -1
+  while i < (my_words.length) / 2
+    last_letter = my_words[i]
+    my_words[i] = my_words[j]
+    my_words[j] = last_letter
+    i += 1
+    j -= 1
+  end
+  return my_words
+  # need to do this x-length of times? x = number of letters in the word/string
 end
+
+word = reverse_words("Engineer")
+puts "#{word}"
