533c927bc02dae6f7ae85f93de9c5ed2b9f6f227
Maria Wissler <wmcarmelina@gmail.com>
2019-05-01 22:37:54 -0700

    words reverse

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -2,5 +2,28 @@
 # Time complexity: ?
 # Space complexity: ?
 def reverse_words(my_words)
-  raise NotImplementedError
+  return nil if my_words == nil
+
+  i = 0
+  m = 0
+  j = my_words.length
+
+  i += 1 while my_words == " "
+
+  while i < j
+    while my_words[i] != " " && i != j
+      i += 1
+    end
+
+    k = i
+    while m < k
+      temp = my_words[k - 1]
+      my_words[k - 1] = temp
+      m += 1
+      k -= 1
+    end
+
+    m = i
+  end
+  return my_words
 end
